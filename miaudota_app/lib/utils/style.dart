441f811af459e43e05357e28f4d16406644510bd
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-07 08:11:02 -0300

    add util definitions for theme

---
+++ miaudota_app/lib/utils/style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static final Color colorCyan = Colors.cyan;
+  static final Color colorCyanEightHundred = Colors.cyan[800];
+  static final Color colorCyanNineHundred = Colors.cyan[900];
+  static final Color colorWhite = Colors.white;
+  static final Color colorBlack = Colors.black;
+
+  static final double pullHeight = 50.0;
+}
