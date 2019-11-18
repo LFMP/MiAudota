9758355cde39f1ce03b1d918acfbfd5c84a9fe7c
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-11-17 21:50:34 -0300

    tela de login montada

---
+++ miaudota_app/lib/pages/login_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:miaudota_app/utils/style.dart';
+
+class LoginPage extends StatelessWidget {
+  final _userController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: AppStyle.padding,
+        color: AppStyle.colorCyan,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 180,
+              height: 180,
+              child: Image.asset('assets/logoMiaudotaEscrito.png'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextFormField(
+              controller: _userController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'E-mail ou username',
+                labelStyle: TextStyle(
+                  color: AppStyle.colorWhite,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+                color: AppStyle.colorWhite,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                labelStyle: TextStyle(
+                  color: AppStyle.colorWhite,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+                color: AppStyle.colorWhite,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: AppStyle.colorWhite,
+                borderRadius: const BorderRadius.all(Radius.circular(60)),
+              ),
+              child: SizedBox(
+                child: FlatButton(
+                  child: Container(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: AppStyle.colorCyanEightHundred,
+                      ),
+                    ),
+                  ),
+                  onPressed: () => print('login'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: AppStyle.colorWhite,
+                borderRadius: const BorderRadius.all(Radius.circular(60)),
+              ),
+              child: SizedBox(
+                child: FlatButton(
+                  child: Container(
+                    child: Text(
+                      'Cadastrar-se',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: AppStyle.colorCyanEightHundred,
+                      ),
+                    ),
+                  ),
+                  onPressed: () => print('cadastro'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- miaudota_app/lib/utils/style.dart
+++ miaudota_app/lib/utils/style.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppStyle {
+  static const padding = EdgeInsets.only(top: 60, left: 40, right: 40);
   static const Color colorCyan = Colors.cyan;
   static const Color colorCyanEightHundred = Color(0xFF00838F);
   static const Color colorCyanNineHundred = Color(0xFF006064);
