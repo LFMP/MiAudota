99ad2dc7f054d18b556e786f8ec0dda1ec95355f
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-12-05 10:44:25 -0300

    validação na ui de login

--- miaudota_app/lib/pages/login_form.dart
+++ miaudota_app/lib/pages/login_form.dart
@@ -19,11 +19,12 @@ class _LoginFormState extends State<LoginForm> {
   _LoginFormState(this.userRepository);
   final _userController = TextEditingController();
   final _passwordController = TextEditingController();
-
   final UsuarioRepository userRepository;
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
     void _onLoginButtonPressed() {
       BlocProvider.of<LoginBloc>(context).add(
         LoginButtonPressed(
@@ -48,114 +49,131 @@ class _LoginFormState extends State<LoginForm> {
         return Container(
           padding: AppStyle.padding,
           color: AppStyle.colorCyan,
-          child: ListView(
-            children: <Widget>[
-              SizedBox(
-                width: 180,
-                height: 180,
-                child: Image.asset('assets/logoMiaudotaEscrito.png'),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                controller: _userController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'E-mail ou username',
-                  labelStyle: TextStyle(
-                    color: AppStyle.colorWhite,
-                    fontWeight: FontWeight.w400,
+          child: Form(
+            key: _formKey,
+            autovalidate: true,
+            onChanged: () => _formKey.currentState.validate(),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(
+                  width: 180,
+                  height: 180,
+                  child: Image.asset('assets/logoMiaudotaEscrito.png'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                TextFormField(
+                  controller: _userController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail ou username',
+                    labelStyle: TextStyle(
+                      color: AppStyle.colorWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'O campo não pode ser vazio';
+                    }
+                    return null;
+                  },
+                  style: const TextStyle(
                     fontSize: 20,
-                  ),
-                ),
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: AppStyle.colorWhite,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(
                     color: AppStyle.colorWhite,
-                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(
+                      color: AppStyle.colorWhite,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'O campo não pode ser vazio';
+                    }
+                    return null;
+                  },
+                  style: const TextStyle(
                     fontSize: 20,
+                    color: AppStyle.colorWhite,
                   ),
                 ),
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: AppStyle.colorWhite,
+                const SizedBox(
+                  height: 40,
                 ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  color: AppStyle.colorWhite,
-                  borderRadius: BorderRadius.all(Radius.circular(60)),
-                ),
-                child: SizedBox(
-                  child: FlatButton(
-                    child: Container(
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: AppStyle.colorCyanEightHundred,
-                        ),
-                      ),
-                    ),
-                    onPressed: () => _onLoginButtonPressed(),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: AppStyle.colorWhite,
+                    borderRadius: BorderRadius.all(Radius.circular(60)),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  color: AppStyle.colorWhite,
-                  borderRadius: BorderRadius.all(Radius.circular(60)),
-                ),
-                child: SizedBox(
-                  child: FlatButton(
-                    child: Container(
-                      child: Text(
-                        'Cadastrar-se',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: AppStyle.colorCyanEightHundred,
+                  child: SizedBox(
+                    child: FlatButton(
+                      child: Container(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: AppStyle.colorCyanEightHundred,
+                          ),
                         ),
                       ),
-                    ),
-                    onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-                      SlideRoute(
-                        page: CadastroPage(
-                          userRepository: userRepository,
-                        ),
-                        direction: SlideDirection.RIGHT_LEFT,
-                      ),
-                      (_) => true,
+                      onPressed: () => _onLoginButtonPressed(),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                child: state is LoginLoading
-                    ? const CircularProgressIndicator()
-                    : null,
-              ),
-            ],
+                const SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: AppStyle.colorWhite,
+                    borderRadius: BorderRadius.all(Radius.circular(60)),
+                  ),
+                  child: SizedBox(
+                    child: FlatButton(
+                      child: Container(
+                        child: Text(
+                          'Cadastrar-se',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: AppStyle.colorCyanEightHundred,
+                          ),
+                        ),
+                      ),
+                      onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                        SlideRoute(
+                          page: CadastroPage(
+                            userRepository: userRepository,
+                          ),
+                          direction: SlideDirection.RIGHT_LEFT,
+                        ),
+                        (_) => true,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: state is LoginLoading
+                      ? const CircularProgressIndicator()
+                      : null,
+                ),
+              ],
+            ),
           ),
         );
       }),
